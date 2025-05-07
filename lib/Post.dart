ef132200a6cbd471c81e2320c9a2ca2589b2eda4
“Nabin <pona_bca2079@lict.edu.np>
2025-05-07 09:17:45 +0545

    Post From API

--- lib/Post.dart
+++ lib/Post.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class Postpage extends StatefulWidget {
@@ -8,33 +9,49 @@ class Postpage extends StatefulWidget {
 }
 
 class _PostpageState extends State<Postpage> {
-
+  List posts = [];
 
   fetchTodos() async {
     final Dio dio = Dio();
     final response = await dio.get(
       'https://jsonplaceholder.typicode.com/posts',
     );
-
-   
+    setState(() {
+      posts = response.data;
+    });
   }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTodos();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: Text("Posts",style: TextStyle(color: Colors.white),),
-      backgroundColor: Colors.red,
-      centerTitle: true,),
-      body: Column(
-        children: [
-         ListTile(
-          title: Text("User ID"),
-          subtitle: Text("Body")  ,
-          leading:Text("Title"),
-          
-          trailing: Text("Body"),
-         )
-          )
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Posts",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: (context, index) {
+          final post = posts[index];
+          final body = post['body'];
+          final trimmedBody = body.length > 50 ? body.substring(0, 47) + "..." : body;
+          return ListTile(
+            
+            subtitle: Text("Body: $trimmedBody"),
+            title: Text(post['title']),
+            leading: Text(post['id'].toString()),
+            // trailing: Text(post['title']),
+          );
+        },
       ),
     );
   }
